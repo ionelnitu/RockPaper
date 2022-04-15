9bf28324a854f41c8f54055658ee7b39c85523f2
ionelnitu <96429720+DeathlyHallowsJava@users.noreply.github.com>
2022-04-15 19:53:47 +0300

    Update Main.java

--- src/Aplication/Main.java
+++ src/Aplication/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,6 +17,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Scene scene=new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("resources/mainFrame.fxml")));
+        Image image=new Image("resources/stageIcon.png");
+        stage.setTitle("Rock Paper Scissors Game");
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
